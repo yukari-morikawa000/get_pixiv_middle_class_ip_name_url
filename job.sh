0e110ae8c4149d886c93bff61e70ef92ca3b9fad
Yukari Morikawa <yukichi@YukarinoMacBook-Air.local>
2025-11-29 13:28:54 +0900

    fix yaml

--- job.sh
+++ job.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# 5つの引数を正しく受け取る
 JOB_NAME="$1"
 NAMESPACE="$2"
 PROJECT_ID="$3"
 DATASET="$4"
-IMAGE="$5"
+IMAGE="$5" # Digdagから渡されたDockerイメージパス
 
 cat <<EOF > job.yaml
 apiVersion: batch/v1
@@ -15,10 +16,14 @@ metadata:
   namespace: ${NAMESPACE}
 spec:
   template:
+  # コンテナが使用するサービスアカウント
+    serviceAccountName: collectro
     spec:
       containers:
       - name: pixiv-ip
-        image: ${IMAGE}
+        image: ${IMAGE} # Digdagから渡されたIMAGE変数を使用
+        # unified_pixiv_search.py の main() を実行
+        command: ["python", "unified_pixiv_search.py"]
         env:
         - name: GCP_PROJECT_ID
           value: "${PROJECT_ID}"
